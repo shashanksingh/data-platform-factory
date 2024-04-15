08e494b4daabd9bcfb2d478408c853c4f7dc661e
shashank singh <shashank.personal@gmail.com>
2024-04-15 09:45:31 +0100

    added
    https://github.com/pydantic/pydantic/discussions/2410

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,13 +1,20 @@
+from abc import abstractmethod
 from typing import Dict
+import abc
 
 from pydantic import BaseModel, ConfigDict
 
 EXTRACT_TYPE: Dict = {}
 
 
-class Extract(BaseModel):
+class Extract(BaseModel, abc.ABC):
     model_config = ConfigDict(extra="forbid")
     source: str
+
+    @property
+    @abc.abstractmethod
+    def template(self):
+        """ The template for the extract"""
 
     def __str__(self):
         return self.source
--- src/load/load.py
+++ src/load/load.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel, ConfigDict
+import abc
 
 
 class Load(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
     destination: str
+
+    @property
+    @abc.abstractmethod
+    def template(self):
+        """ The template for the extract"""
 
     def __str__(self):
         return self.destination
